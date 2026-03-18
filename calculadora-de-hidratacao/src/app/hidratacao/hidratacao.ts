f90cd1942ee7c897a8de34cd16c85b26d2aa0d91
Júlia L. W. <julia_wecolovis@estudante.sesisenai.org.br>
2026-03-18 16:16:15 -0300

    a

--- calculadora-de-hidratacao/src/app/hidratacao/hidratacao.ts
+++ calculadora-de-hidratacao/src/app/hidratacao/hidratacao.ts
@@ -12,19 +12,20 @@ export class Hidratacao {
   peso: number = 0;
   atividade: string = 'sedentario';
   hidratacao: string = '';
-  resultado = number = 0;
+  litros: number = 0;
+  garrafas: number = 0;
   calcularHidratacao() {
     if (this.peso > 0) {
       let base = this.peso/35;
       if (this.atividade === 'sedentário') {
-        this.resultado = base*1.0;
+        this.litros = base*1.0;
       } else if (this.atividade === 'moderado') {
-        this.resultado = base*1.2;
+        this.litros = base*1.2;
       }
       else if (this.atividade === 'intenso') {
-        this.resultado = base*1.5;
+        this.litros = base*1.5;
       }
-      let garrafas = Math.ceil(this.resultado/0.5);
+      this.garrafas = Math.ceil(this.litros/0.5);
     }
   }
 }
